2532dd70992655de52eca94ce3118f9dc1a07b38
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-05-29 00:26:33 +0300

    finish

--- api.py
+++ api.py
@@ -20,8 +20,15 @@ class PetFriends:
         return status, result
 
     def get_list_of_pets(self, auth_key, filter):
-        headers = {'auth_key': auth_key}
+        headers = {'auth_key': auth_key['key']}
         filter = {'filter': filter}
 
         res = requests.get(self.base_url+'/api/pets', headers=headers, params=filter)
 
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
