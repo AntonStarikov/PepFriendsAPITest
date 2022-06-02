bd20f34bea9e6891222d064cd42d739b31467ce9
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-06-02 14:38:08 +0300

    redact autotests 9 new test

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -42,7 +42,6 @@ def test_successful_delete_self_pet():
 
     pet_id = my_pets['pets'][0]['id']
     status, _ = pf.delete_pet(auth_key, pet_id)
-
     _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
     assert status == 200
     assert pet_id not in my_pets.values()
@@ -70,39 +69,42 @@ def test_add_pets_with_valid_data_without_photo(name = 'Кто я', animal_tipe 
     assert result['name'] == name
 
 def test_get_api_key_for_no_valid_email(email = 'New@rambler.ru', password = valid_password):
-    """ Проверка, негативный тест, ввод неверного e-mail """
+    """ Проверка, негативное тестирование, ввод неверного e-mail """
 
     status, result = pf.get_api_key(email, password)
     assert status == 403
     assert 'key' not in result
 
 def test_get_api_key_for_no_valid_password(email=valid_email, password='159753'):
-    """ Проверка, негативный тест, ввод неверного пароля """
+    """ Проверка, негативное тестирование, ввод неверного пароля """
 
     status, result = pf.get_api_key(email, password)
     assert status == 403
     assert 'key' not in result
 
-def test_add_new_pet_with_no_valid_age(name = 'Кэмел', animal_type = 'верблюд', age = 'вот это чушь', pet_photo = 'images/Dog1.jpg'):
-    """ Проверка добавления питомца с вводом нечисловой переменной в поле возраста"""
+def test_add_new_pet_with_no_valid_age_str(name = 'Кэмел', animal_type = 'верблюд', age = 'вот это чушь', pet_photo = 'images/Dog1.jpg'):
+    """ Проверка, негативное тестирование, добавления питомца с вводом нечисловой переменной в поле возраста"""
 
     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
-    assert status == 400
+    assert status == 200
     assert age not in result['age']
 
-def test_add_new_pet_with_four_age_nmber(name = 'Рой', animal_type = 'Барсук', age = '1234', pet_photo = 'images/Dog1.jpg'):
-    """ Проверка добавление питомца с четырехзначным числом в поле возраст"""
+def test_add_new_pet_with_four_age_number(name = 'Рой', animal_type = 'Барсук', age = '1234', pet_photo = 'images/Dog1.jpg'):
+    """ Проверка, негативное тестирование, добавление питомца с четырехзначным числом в поле возраст """
 
     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
-    assert status == 400
+    assert status == 200
     assert len(result['age']) < 4, 'Питомец добавлен на сайт с четырехзначным числом в поле возраст!'
 
 def test_add_pet_with_value_in_variable_name(name='', animal_type='кот', age='2', pet_photo='images/Dog1.jpg'):
-    '''Проверка, негативное тестирование,  возможности добавления питомца без имени'''
+    """ Проверка, негативное тестирование,  возможности добавления питомца без имени """
+
     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
 
     _, api_key = pf.get_api_key(valid_email, valid_password)
@@ -111,7 +113,7 @@ def test_add_pet_with_value_in_variable_name(name='', animal_type='кот', age=
     assert result['name'] != '', 'Питомец добавлен на сайт с пустым значением в имени'
 
 def test_add_pet_with_a_lot_of_variable_name(animal_type='терьер', age='6', pet_photo='images/Dog1.jpg'):
-    '''Проверка, негативное тестирование, добавления питомца с именем состоящим более 5 слов'''
+    """ Проверка, негативное тестирование, добавления питомца с именем состоящим более 5 слов """
 
     name = 'Просто очееень длинное имя которое никогда не существовало'
 
@@ -119,12 +121,21 @@ def test_add_pet_with_a_lot_of_variable_name(animal_type='терьер', age='6'
     status, result = pf.add_new_pet(api_key, name, animal_type, age, pet_photo)
 
     list_name = result['name'].split()
-
     assert status == 200
     assert len(list_name) < 5, 'Питомец добавлен с именем более 5 слов'
 
+def test_add_new_pet_with_no_valid_tipe_int(name = 'Боря', animal_type = '849256', age = '12', pet_photo = 'images/Dog1.jpg'):
+    """ Проверка, негативное тестирование, добавления питомца с вводом числа в поле порода"""
+
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+    assert status == 200
+    assert animal_type not in result['animal_type'], 'Питомец добавлен на сайт с цифрами вместо породы!'
+
 def test_add_pet_with_a_lot_of_variable_tipe(name ='Афоня', age='41', pet_photo='images/Dog1.jpg'):
-    '''Проверка, негативное тестирование, добавления питомца с названием породы состоящим более 5 слов'''
+    """ Проверка, негативное тестирование, добавления питомца с названием породы состоящим более 5 слов """
 
     animal_type = 'Такой породы просто не может существовать на этом свете'
 
@@ -132,7 +143,6 @@ def test_add_pet_with_a_lot_of_variable_tipe(name ='Афоня', age='41', pet_p
     status, result = pf.add_new_pet(api_key, name, animal_type, age, pet_photo)
 
     list_name = result['animal_type'].split()
-
     assert status == 200
     assert len(list_name) < 5, 'Питомец добавлен с названием породы более 5 слов'
 
